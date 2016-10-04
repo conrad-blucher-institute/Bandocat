aca88ab14a967b0c772e07aef21aa6eaa6496988
Xuan27 <jmshebar259@gmail.com>
2016-10-04 14:05:05 -0500

    Collection Menu Template Style -css code was added to the master css file -The structure of the collection template was created.

--- Templates/Map/index.php
+++ Templates/Map/index.php
@@ -10,13 +10,63 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
         <title>Welcome to BandoCat!</title>
-
+        <link rel = "stylesheet" type = "text/css" href = "CSS/Map_Collection.css" >
         <link rel = "stylesheet" type = "text/css" href = "../../Master/master.css" >
         <script type="text/javascript" src="ExtLibrary/jQuery-2.2.3/jquery-2.2.3.min.js"></script>
 
     </head>
     <body>
-<?php include '../../Master/header.php'; ?>
+    <table id = "thetable">
+        <tr>
+            <td class="menu_left" id="thetable_left">
+                <?php include '../../Master/header.php';
+                include '../../Master/sidemenu.php' ?>
+            </td>
+            <td class="Collection" id="thetable_right">
+                    <h2>Collection Menu</h2>
+                    <table class="Collection_Table">
+                        <tr>
+                            <td>
+                                <h4 class="Collection_Title">Collection Name</h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="Collection_data">
+                                <input type="button" class="Collection_Button" value="Input Map Information"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="Collection_data">
+                                <input type="button" class="Collection_Button" value="View Map Information" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="Collection_data">
+                                <input type="button" class="Collection_Button" value="View Tickets" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="Collection_data">
+                                <input type="button" class="Collection_Button" value="Utilities" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="Collection_data">
+                                <input type="button" class="Collection_Button" value="Submit a Ticket" />
+                            </td class="Collection_data">
+                        </tr>
+                        <tr>
+                            <td class="Collection_data">
+                                <input type="button" class="Collection_Button" value="Change My Password" />
+                            </td>
+                        </tr>
+                </table>
+            </td>
+        </tr>
+
+    </table>
+
+    <?php include '../../Master/footer.php'; ?>
 
     </body>
 </html>
