8eda08a1b58e005bbb40e0d58a7c48bb73551537
Xuan27 <jmshebar259@gmail.com>
2017-01-25 13:04:30 -0600

    Announcement Store file created.

--- Forms/Main/announcement_store.php
+++
@@ -1,9 +0,0 @@
-<?php
-$myFile = "announcement.json";
-$fh = fopen($myFile, 'w') or die("can't open file");
-$stringData = $_GET["announcement"];
-$announcement = array();
-$announcement = json_encode($stringData);
-fwrite($fh, $announcement);
-fclose($fh)
-?>
