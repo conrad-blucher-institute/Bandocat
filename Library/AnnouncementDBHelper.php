b10b5c5f6f895db77364bbcf7c89a218c995d3d8
Son <sd.nguyen91@gmail.com>
2017-02-03 12:50:51 -0600

    Resolve merge conflicts

---
+++ Library/AnnouncementDBHelper.php
@@ -0,0 +1,21 @@
+<?php
+//NOTE:
+/*
+- BEFORE EXECUTING QUERIES OR CALL SPs, MUST EITHER:
+    + Use prepare statement
+    + OR Sanitize input using mysqli_real_escape_string
+
+- BIND PARAMETER: Types: s = string, i = integer, d = double,  b = blob
+*/
+
+//PLEASE USE THIS TEMPLATE TO COMMENT ON FUNCTIONS
+/**********************************************
+Function:
+Description:
+Parameter(s):
+Return value(s):
+ ***********************************************/
+class AnnouncementDBHelper
+{
+
+}
