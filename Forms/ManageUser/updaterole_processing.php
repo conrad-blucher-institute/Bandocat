7f6320111bf95065b7b836f31b1e560a0818deb5
GISJohnECS <gisjohnecs@gmail.com>
2017-02-21 12:04:18 -0600

    forms/manage users: -index.php -resetpassword_processing.php * -updaterole_processing.php
    Library/DBHelper:
    -GET_USER_TABLE
    -USER_ROLE_UPDATE
    -USER_ADMIN_PASSWORD_RESET

--- Forms/ManageUser/updaterole_processing.php
+++ Forms/ManageUser/updaterole_processing.php
@@ -2,10 +2,11 @@
 //for admin use only
 include '../../Library/SessionManager.php';
 $session = new SessionManager();
-/*if($session->isAdmin() == false) {
+if($session->isAdmin() == false) {
     header('Location: ../../');
-} */
+} 
 require('../../Library/DBHelper.php');
 $DB = new DBHelper();
-$ret = $DB->USER_ROLE_UPDATE($_POST['ddl_user'],$_POST['rd_Role']);
+$ret = $DB->USER_ROLE_UPDATE($_POST['ddl_user'],$_POST['
+']);
 echo $ret;
