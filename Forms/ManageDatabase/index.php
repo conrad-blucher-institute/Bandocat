b2da4677773ab5a544f9aea11118b3f6e0470fd8
Ruben3rd <rortiz18@islander.tamucc.edu>
2019-05-29 12:53:23 -0500

    - must be super admin for database manager access

--- Forms/ManageDatabase/index.php
+++ Forms/ManageDatabase/index.php
@@ -9,7 +9,7 @@
 include '../../Library/SessionManager.php';
 $session = new SessionManager();
 $test = [1, 2, 3];
-if($session->isAdmin()) {
+if($session->isSuperAdmin()) {
     require('../../Library/DBHelper.php');
     $DB = new DBHelper();
 }
