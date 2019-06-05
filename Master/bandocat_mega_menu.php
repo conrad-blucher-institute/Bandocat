59240cf8b0f7a9c23f2910fe60952c215bab8c7a
Ruben3rd <rortiz18@islander.tamucc.edu>
2019-06-05 12:25:00 -0500

    - padding for mega menu

--- Master/bandocat_mega_menu.php
+++ Master/bandocat_mega_menu.php
@@ -274,7 +274,7 @@ $errorTickets .= $tempt;
         <!-- Website Errors -->
         <ul class="navbar-nav ml-auto">
             <!-- Translation buttons -->
-            <div class="pt-2 pl-1" id="google_translate_element"></div>
+            <div class="pt-2 pr-2" id="google_translate_element"></div>
             <!-- Bug Reporting (Website errors) -->
             <li class="nav-item" data-toggle="modal" data-target="#bugReportModal">
                 <a class="nav-link">
