94250bdb6c9a93a17b370d036da7b5934f52f1f1
Ruben3rd <rortiz18@islander.tamucc.edu>
2019-06-19 11:57:32 -0500

    - added loading modal file to master

---
+++ Master/load.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rortiz18
+ * Date: 6/19/2019
+ * Time: 11:55 AM
+ */
+?>
+<!-- Loading Modal -->
+<div class="modal" id="loaderModal" tabindex="-1" role="dialog" aria-labelledby="loaderModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document" style="width: 15em;">
+        <div class="modal-content" id="loaderModalContent">
+
+        </div>
+    </div>
+</div>
