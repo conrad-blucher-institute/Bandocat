0a91e7d354fc28d7dddd6ac13f6ee21d986b43c5
Henry <hreeves@islander.tamucc.edu>
2019-06-27 11:26:36 -0500

    Adding GDAL for georectification

--- GeoRec/Map/php/tileCreator.php
+++ GeoRec/Map/php/tileCreator.php
@@ -79,7 +79,7 @@ $imageInfo = array(
 	//compute and run shell command to create tiles
 	$zoom = log(max($imageInfo['width'], $imageInfo['height'])/256, 2);
 	$zoom = ceil($zoom);
-	$command = "python ../../../ExtLibrary/GDAL/gdal2tiles-multiprocess.py -l -p raster -z 0-" . $zoom . " -w none -e " . $imagepath . " ../Temp/Tiles/". $imageInfo['tempSubDirectory'];
+	$command = "python ../ExtLibrary/GDAL/gdal2tiles-multiprocess.py -l -p raster -z 0-" . $zoom . " -w none -e " . $imagepath . " ../Temp/Tiles/". $imageInfo['tempSubDirectory'];
 	exec($command,$output,$ret);
 	//echo $command . "<br>";
 	//print_r(array($output,$ret)); //use this to debug $command
